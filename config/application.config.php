b69c2a9ce11b9f683800b7c4d3c906cc3e753bce
Alex Savchenko <a.savchenko@codetiburon.com>
2013-10-11 14:58:05 +0300

    added modules local configuration

--- config/application.config.php
+++ config/application.config.php
@@ -3,6 +3,18 @@ return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
         'Application',
+        'DoctrineModule',
+        'DoctrineORMModule',
+        'ZfcBase',
+        'ZfcUser',
+        'ZfcAdmin',
+        'BjyAuthorize',
+        'ZendDeveloperTools',
+        'ZfcTwitterBootstrap',
+        'OcraServiceManager',
+        'OcraCachedViewResolver',
+        'SxMail',
+        'ZfcUserDoctrineORM',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
